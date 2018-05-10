15a8638628abb5fde81e7fcad03f4fe5cb36a6e3
gabor-szeles <szelesgbr@gmail.com>
2018-05-10 17:02:11 +0200

    added dto for street tags

---
+++ src/main/java/org/projectargus/argus_server/tags/StreetTagDto.java
@@ -0,0 +1,24 @@
+package org.projectargus.argus_server.tags;
+
+public class StreetTagDto {
+
+    private String userName;
+
+    private String imageData;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getImageData() {
+        return imageData;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData;
+    }
+}
